7091651dc65e671fa1ef143aeb2426c74cb46211
Julien Richard [RMT - JS] Wild Code School <33219171+jujuck@users.noreply.github.com>
2023-04-07 13:41:51 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,12 @@
 import Home from "./pages/Home";
+// US 1 import
+
+// US 2 Import
+
+// US 3 import
+
+// US 4 import
+
 
 import "./App.css";
 
@@ -6,6 +14,10 @@ function App() {
   return (
     <div className="App">
       <Home />
+      {/** US 1 Page */}
+      {/** US 2 Page */}
+      {/** US 3 Page */}
+      {/** US 4 Page */}
       <p>coucou</p>
     </div>
   );
