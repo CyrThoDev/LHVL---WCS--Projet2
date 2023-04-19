70212281162c5b18fcf7a69901a9461c23d2534c
CYRIELLE THOMAS <cyriellethomas.pro@gmail.com>
2023-04-19 15:22:02 +0200

    creation contact form et début css contact form

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import "../sass/contact.scss";
 
 function Contact() {
-  return <div>Contact</div>;
+  const [name, setName] = useState("Your name");
+  const [email, setEmail] = useState("Your email");
+  const [message, setMessage] = useState("Your message");
+
+  return (
+    <>
+      <div>Contact</div>
+      <form className="form-container">
+        <label>
+          Enter your name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Enter your Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Enter your Message:
+          <input
+            type="textarea"
+            value={message}
+            rows={5}
+            cols={5}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default Contact;
