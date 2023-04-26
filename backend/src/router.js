8e0b6df8dacb56f464c2f609c602210a65ef0196
CYRIELLE THOMAS <cyriellethomas.pro@gmail.com>
2023-04-26 12:11:34 +0200

    Creation database.js and connection router.js to database

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,12 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
+const connection = require("./database");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+connection
+  .getConnection()
+  .then(() => {
+    console.info("Can reach database");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// const itemControllers = require("./controllers/itemControllers");
+// router.get("/items", itemControllers.browse);
+// router.get("/items/:id", itemControllers.read);
+// router.put("/items/:id", itemControllers.edit);
+// router.post("/items", itemControllers.add);
+// router.delete("/items/:id", itemControllers.destroy);
 
 module.exports = router;
