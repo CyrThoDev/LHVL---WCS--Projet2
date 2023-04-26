804a51b9b33e4303e56a0cbeaa172a587ccd2925
CYRIELLE THOMAS <cyriellethomas.pro@gmail.com>
2023-04-26 13:58:58 +0200

    Addition Route home and Superhero

--- backend/src/router.js
+++ backend/src/router.js
@@ -13,6 +13,22 @@ connection
     console.error(err);
   });
 
+router.get("/", (req, res) => {
+  res.status(200);
+});
+
+router.get("/superhero", (req, res) => {
+  connection
+    .query("SELECT * FROM superhero")
+    .then(([results]) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+});
+
 // const itemControllers = require("./controllers/itemControllers");
 // router.get("/items", itemControllers.browse);
 // router.get("/items/:id", itemControllers.read);
